59e93116852395daf1c9538a0a20cdaa7074a4f3
Syed Muhammad Hammad Ghani <hammy.pk30@gmail.com>
2021-08-25 20:00:13 +0500

    Done

---
+++ android/app/src/main/kotlin/com/example/fade_widget_in_out/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fade_widget_in_out
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const appTitle = "Animated Opacity";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: appTitle));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({required this.title, Key? key}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _visible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(milliseconds: 500),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toggle Opacity',
+        child: Icon(Icons.ac_unit),
+      ),
+    );
+  }
+}
